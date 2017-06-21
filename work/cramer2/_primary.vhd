42cf8f99ad4ca9fd9cbde8f1fecab98fa2a7ef7c
Nicholas Jens <nnj2435@gmail.com>
2017-06-21 14:17:39 -0700

    Create _primary.vhd

---
+++ work/cramer2/_primary.vhd
@@ -0,0 +1,12 @@
+library verilog;
+use verilog.vl_types.all;
+entity cramer2 is
+    port(
+        HEX0            : out    vl_logic;
+        HEX1            : out    vl_logic;
+        HEX2            : out    vl_logic;
+        HEX4            : out    vl_logic;
+        HEX5            : out    vl_logic;
+        HEX6            : out    vl_logic
+    );
+end cramer2;
